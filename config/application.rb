b7284bc8b78bf30e47e72f9f56efbd00136f51ac
hiroishi37 <57392511+hiroishi37@users.noreply.github.com>
2019-11-24 08:52:41 +0900

    edit front

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,4 @@ module MeshiBlog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  config.time_zone = 'Tokyo'
 end
