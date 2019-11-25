9a59519ee4f1cd49108dd31b99432eeacdf496d2
hiroishi37 <57392511+hiroishi37@users.noreply.github.com>
2019-11-25 16:44:51 +0900

    updata

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "comments#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "meshis#index"
+  resources :users, only: [:edit, :update]
+  resources :meshis, only: [:new, :create, :edit, :update] do
+    resources :comments, only: [:index, :create]
+  end
 end
